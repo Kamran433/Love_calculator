67b5f6b077fafc2bcdd9c83d97309b2446f7b290
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:27:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+# d=name1 + name2
+# b=d.lower()
+# c=b.count("l")
+# e=b.count("o")
+# f=b.count("v")
+# g=b.count("e")
+# k=c+e+f+g
+# d=b.count("t")
+# l=b.count("r")
+# m=b.count("u")
+# n=b.count("e")
+# o=d+l+m+n
+# like=int(str(o)+str(k))
+# if like>90 or like<10:
+#     print(f"Your score is {like}, you go together like coke and mentos.")
+# elif like>=40 and like<=50:
+#         print(f"Your score is {like}, you are alright together.")
+# else:
+#             print(f"Your score is {like}.")
